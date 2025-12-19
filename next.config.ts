caa63f1bca4c792c71e77e1b5a192ce3e82cb929
DevExcel Developer <developer@devexcelit.com>
2025-12-19 12:31:15 +0500

    changes

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,20 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ["192.168.20.177"],
+  serverExternalPackages: [
+    "keyv",
+    "@keyv/redis",
+    "@keyv/mongo",
+    "@keyv/sqlite",
+    "@keyv/postgres",
+    "@keyv/mysql",
+    "@keyv/etcd",
+    "@keyv/offline",
+    "@keyv/tiered",
+    "cacheable-request",
+    "got",
+    "ably",
+  ],
 };
 
 export default nextConfig;
